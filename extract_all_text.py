1450a785c33407d8925d17d105c478e1164d901a
meijiishinislame <meijiishinislame@gmail.com>
2025-08-16 16:36:54 -1000

    still buggy, but fixed some repetetive functions

--- extract_all_text.py
+++ extract_all_text.py
@@ -24,6 +24,10 @@ def get_character_name_from_bytes(bytes):
             break
     return character_name
 
+
+
+
+
 ############################################
 
 
@@ -55,13 +59,25 @@ def load_ignore_list():
 
 
 
+
+
+
+
+
 config = load_config()
 markers = load_markers()
 ignore_list = load_ignore_list()
 
-print(config)
-print(markers)
-print(ignore_list)
+#print(config)
+#print(markers)
+#print(ignore_list)
+
+
+
+
+
+
+
 
 
 
@@ -79,6 +95,12 @@ class Marker:
         return hash((self.position, self.length))
 
 
+
+
+
+
+
+
 class DialogScene:
     def __init__(self, start_address, end_address, input_filename, output_filename):
         self.start_address = int(start_address, 16)
@@ -89,11 +111,15 @@ class DialogScene:
         self.marker_positions = set()
         self.byte_stream = self.get_byte_stream()
         self.full_bytes = self.get_full_filebytes()
+        
+        
 
     def get_full_filebytes(self):
         with open(self.input_filename, "rb") as f:
             file_bytes = io.BytesIO(f.read())
         return file_bytes
+    
+    
     
     def get_byte_stream(self):
         dialog_bytes_length = self.end_address - self.start_address
@@ -101,6 +127,70 @@ class DialogScene:
             f.seek(self.start_address)
             bytes = f.read(dialog_bytes_length)
         return io.BytesIO(bytes)
+        
+        
+        
+    def get_possible_match_positions(self, byte_value, byte_stream_to_search):
+        possible_match_positions = []
+        first_byte = byte_value[:1]
+        byte_stream_to_search.seek(0)
+        byte_data = True
+                
+        while byte_data:
+            byte_data = byte_stream_to_search.read(1)
+            if byte_data == first_byte:
+                possible_match_positions.append(byte_stream_to_search.tell())
+        return possible_match_positions
+    
+    
+    
+    def get_positions(self, byte_value, possible_match_positions, byte_stream_to_search):
+        positions = set()
+        byte_stream_to_search.seek(0)
+        read_length = len(byte_value)
+        for i in possible_match_positions:
+            byte_stream_to_search.seek(i-1)
+            bytes_to_check = byte_stream_to_search.read(read_length)
+            #print("position", byte_stream_to_search.tell(), "expected position", i, "bytes to check", bytes_to_check.hex(), "byte value", byte_value.hex())
+            if bytes_to_check == byte_value:
+                positions.add(Marker(position=i-1, length=read_length)) 
+        return positions
+
+
+
+    def get_marker_positions(self):
+        #we look one byte at a time to get the list of possible positions first
+        #then, we check again at all these positions to make sure if theyre actual matches or not        
+        for marker in markers:
+            byte_value = marker["value"]
+            possible_match_positions = self.get_possible_match_positions(byte_value, self.byte_stream)                    
+            self.byte_stream.seek(0)
+            positions = set()    
+            if possible_match_positions:
+                for marker in markers:
+                    positions = self.get_positions(byte_value, possible_match_positions, self.byte_stream)
+                self.marker_positions.update(positions)
+        sorted_marker_positions = sorted(self.marker_positions, key=lambda m: m.position)
+        self.marker_positions = sorted_marker_positions
+             
+        
+    def get_ignore_positions(self):
+        #we look one byte at a time to get the list of possible positions first
+        #then, we check again at all these positions to make sure if theyre actual matches or not        
+        for byte_value in ignore_list:
+            possible_match_positions = self.get_possible_match_positions(byte_value, self.byte_stream)
+                    
+            self.byte_stream.seek(0)
+            
+            positions = set() 
+            if possible_match_positions:
+                for byte_value in ignore_list:
+                    positions = self.get_positions(byte_value, possible_match_positions, self.byte_stream)
+                self.ignore_positions.update(positions)
+        sorted_ignore_positions = sorted(self.ignore_positions, key=lambda m: m.position)
+        self.ignore_positions = sorted_ignore_positions
+    
+    
     
     def find_position_in_main_file(self, bytes):
         possible_match_positions = []
@@ -117,74 +207,12 @@ class DialogScene:
         self.full_bytes.seek(self.start_address)
         
         if possible_match_positions:
-            read_length = len(bytes)
-            for i in possible_match_positions:
-                self.full_bytes.seek(i - 1)
-                bytes_to_check = self.full_bytes.read(read_length)
-                if bytes_to_check == bytes:
-                    position = self.full_bytes.tell()
-                    return position
+            positions = self.get_positions(bytes, possible_match_positions, self.full_bytes)
+            return positions
         else:
             return None
-    
-    def get_ignore_positions(self):
-        #we look one byte at a time to get the list of possible positions first
-        #then, we check again at all these positions to make sure if theyre actual matches or not
-        possible_match_positions = []
-        
-        for hex_value in ignore_list:
-            first_byte = hex_value[:1]
-            self.byte_stream.seek(0)
-            byte_data = 1
-            
-            while byte_data:
-                byte_data = self.byte_stream.read(1)
-                if byte_data == first_byte:
-                    possible_match_positions.append(self.byte_stream.tell())
-                    
-            self.byte_stream.seek(0)
-            
-            if possible_match_positions:
-                for hex_value in ignore_list:
-                    read_length = len(hex_value)
-                    for i in possible_match_positions:
-                        self.byte_stream.seek(i - 1)
-                        bytes_to_check = self.byte_stream.read(read_length)
-                        if bytes_to_check == hex_value:
-                            self.ignore_positions.add(Marker(position=i-1, length=read_length)) 
-        sorted_ignore_positions = sorted(self.ignore_positions, key=lambda m: m.position)
-        self.ignore_positions = sorted_ignore_positions
 
 
-    def get_marker_positions(self):
-        #we look one byte at a time to get the list of possible positions first
-        #then, we check again at all these positions to make sure if theyre actual matches or not
-        possible_match_positions = []
-        
-        for marker in markers:
-            hex_value = marker["value"]
-            first_byte = hex_value[:1]
-            self.byte_stream.seek(0)
-            byte_data = 1
-            
-            while byte_data:
-                byte_data = self.byte_stream.read(1)
-                if byte_data == first_byte:
-                    possible_match_positions.append(self.byte_stream.tell())
-                    
-            self.byte_stream.seek(0)
-            
-            if possible_match_positions:
-                for marker in markers:
-                    hex_value = marker["value"]
-                    read_length = len(hex_value)
-                    for i in possible_match_positions:
-                        self.byte_stream.seek(i - 1)
-                        bytes_to_check = self.byte_stream.read(read_length)
-                        if bytes_to_check == hex_value:
-                            self.marker_positions.add(Marker(position=i-1, length=read_length))  
-        sorted_marker_positions = sorted(self.marker_positions, key=lambda m: m.position)
-        self.marker_positions = sorted_marker_positions
 
     def extract_text(self):
         #header = "start_position,end_position,character_name,sjis_hex,utf-8_text"
@@ -213,8 +241,11 @@ class DialogScene:
                 
 
                 dialog_bytes += self.byte_stream.read(1)
+                
                 if self.byte_stream.tell() >= len(self.byte_stream.getvalue()):
                     dialog = False
+                    
+                    
             dialog_without_last_byte = dialog_bytes[:-1]
             dialog_bytes = dialog_without_last_byte
             character_name_start_position = self.start_address + current_marker.position
@@ -226,6 +257,11 @@ class DialogScene:
             print("dialog_start_position:", f"0x{dialog_start_position:08X}")
             print("dialog_bytes:", dialog_bytes.hex())
             print("Dialog in Shift-JIS:", dialog_bytes.decode('shift_jis', errors='replace'))
+
+
+
+
+################## main ##################################
 
 
 for item in config:
